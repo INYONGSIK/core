52edca776ce1fa70be460cc8ddb0b2b498aef6bf
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-10-02 02:26:52 +0900

    AppConfig를 통한 의존성 주입

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,18 +1,26 @@
 package hello.core.member;
 
 
+import hello.core.order.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
+    MemberService memberService;
+
+
+    @BeforeEach //앱실행전에 무조건 실행
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
     @Test
-    void join(){
-
-        MemberService memberService = new MemberServiceImpl();
-
+    void join() {
         //given
-        Member member = new Member(1L, "memberA",Grade.VIP);
+        Member member = new Member(1L, "memberA", Grade.VIP);
 
         //when
         memberService.join(member);
