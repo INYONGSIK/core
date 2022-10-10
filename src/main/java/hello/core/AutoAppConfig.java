f289eb70b86ee4835857f44399ba89858964842a
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-10-10 22:31:01 +0900

    ComponentScan basePackage

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",  // 탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해 하위 패키지를 모두 탐색
+//        basePackageClasses = AutoAppConfig.class,  //지정한 클래스의 패키지를 탐색 시작 위치로 지정한다.
+        //지정하지않으면 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
