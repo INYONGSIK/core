12741bec3653c7589666c12395c51759ae5500e9
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-10-10 21:50:05 +0900

    ComponentScan and AutoWired

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -1,6 +1,6 @@
 package hello.core.beanfind;
 
-import hello.core.order.AppConfig;
+import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -26,7 +26,7 @@ public class ApplicationContextInfoTest {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-            
+
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name = " + beanDefinitionName + " object= " + bean);
--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -1,8 +1,8 @@
 package hello.core.singleton;
 
+import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberServiceImpl;
-import hello.core.order.AppConfig;
 import hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
